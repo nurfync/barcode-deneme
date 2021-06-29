26a1d977f8ebd31497cfc8292168dee9d4e0fee4
fatmanur <fatmanurafyoncu@gmail.com>
2021-06-29 22:13:59 +0300

    version1

--- src/Screens/Camera.js
+++ src/Screens/Camera.js
@@ -4,57 +4,37 @@ import { RNCamera } from 'react-native-camera'
 import BarcodeMask from 'react-native-barcode-mask';
 const Camera = props => {
     const refCamera = useRef();
-    const [isBarcodeRead, setIsBarcodeRead] = useState(false);
-    const [barcodeType, setBarcodeType] = useState('');
-    const [barcodeValue, setBarcodeValue] = useState('');
 
-    useEffect(() => {
-        if (isBarcodeRead) {
-            Alert.alert(
-                barcodeType,
-                barcodeValue,
-                [
-                    {
-                        text: 'OK',
-                        onPress: () => {
-                            // reset everything
-                            setIsBarcodeRead(false);
-                            setBarcodeType('');
-                            setBarcodeValue('');
-                        }
-                    }
-                ]
-            );
-        }
+const barcodeRecognized = ({ barcodes }) => {
+    barcodes.forEach(barcode => Alert.alert(barcode.data))
 
-    }, [isBarcodeRead, barcodeType, barcodeValue]);
+  };
+  const defaultBarcodeTypes = [];
 
-    const onBarcodeRead = event => {
-        if (!isBarcodeRead) {
-            setIsBarcodeRead(true);
-            setBarcodeType(event.type);
-            setBarcodeValue(event.data);
-        }
-    }
-    const defaultBarcodeTypes = [];
+
     return (
-        <RNCamera
-            ref={rnCameraRef => refCamera.current = rnCameraRef}
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
-            // barcodeTypes={isBarcodeRead ? [] : defaultBarcodeTypes}
-            barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
-            onBarCodeRead={onBarcodeRead}
-            androidCameraPermissionOptions={{
-                title: 'Permission to use camera',
-                message: 'We need your permission to use your camera',
-                buttonPositive: 'Ok',
-                buttonNegative: 'Cancel',
-            }}
-        >
+        <>
+            <RNCamera
+                style={{
+                    flex: 1,
+                    backgroundColor: 'pink'
+                }}
+                onGoogleVisionBarcodesDetected={barcodeRecognized}
 
-            <BarcodeMask />
-        </RNCamera>
+                ref={rnCameraRef => refCamera.current = rnCameraRef}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.on}
+                androidCameraPermissionOptions={{
+                    title: 'Permission to use camera',
+                    message: 'We need your permission to use your camera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+            >
+
+            </RNCamera>
+
+        </>
     );
 };
 
